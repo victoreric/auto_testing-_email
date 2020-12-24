91cc2014d44328c21f4355ce280239af5eb1d544
victorEric <victoreric1409@gmail.com>
2020-12-24 13:05:14 +0700

    revisi

--- tusurMail.py
+++ tusurMail.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from PIL import Image
 import time
+#теста осуществляется через командную строку, браузер chrome
 driver=webdriver.Chrome()
 
 #address
@@ -9,14 +10,16 @@ driver.get('https://e.tusur.ru/')
 #max_window
 driver.maximize_window()
 
-
 #username
 driver.find_element_by_id('username').send_keys('pattiraddzhavane.v.549-m@e.tusur.ru')
 
-#password error
+
 passwd=driver.find_element_by_id('password')
-passwd.send_keys('Sengtau99')
-# passwd.send_keys('12345')
+passwd.send_keys('Pabedi99')
+
+#если пароль неверный, программа запустит команду "except"
+#passwd.send_keys('12345')
+
 #remember
 driver.find_element_by_name('remember').click()
 #press enter or login
@@ -38,6 +41,7 @@ try :
     time.sleep(1)
 
     # message
+    # отправить e-mail. сообщение с темой и текстом(не менее 50 символов), прикрепленным файлом(excel/docx/txt)
     driver.find_element_by_css_selector('.note-editable').send_keys('Автоматизирование тестирование с Selenium и Python \n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum eu purus dictum semper. Donec aliquet nisi quis sapien pulvinar tempus. Sed nec ultricies nibh. Integer eu ante leo. Aenean eros urna, hendrerit vitae bibendum sed, mollis eget nibh. Nunc vitae sagittis urna. Vestibulum varius, justo a luctus luctus, nunc elit gravida sem, sit amet ullamcorper orci neque vitae quam. Cras luctus dapibus hendrerit. \n Nunc iaculis viverra venenatis. In hac habitasse platea dictumst. Nam rutrum neque ut sagittis commodo. Duis turpis quam, luctus in ipsum eget, scelerisque malesuada augue. Nunc scelerisque magna purus, in lobortis est cursus quis. Vivamus convallis sagittis massa. Vivamus eget felis eu orci suscipit imperdiet. Ut vitae hendrerit sapien. Morbi nec convallis erat. Nullam dignissim nisi erat, eu vehicula ligula pellentesque ac.\n Suspendisse potenti. Nullam gravida nulla quis ipsum tincidunt semper. Suspendisse potenti. Praesent neque felis, bibendum quis nibh sed, consequat blandit elit. Vestibulum scelerisque metus eget urna placerat, sit amet faucibus dolor elementum. Nunc vel libero luctus mauris feugiat dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam a erat ultricies, pulvinar nunc quis, tempus lacus. Aliquam nec tempus nibh. Curabitur maximus tempus efficitur. Integer nisl erat, tristique nec sagittis in, maximus non dolor. Proin lorem nibh, ultricies cursus suscipit sit amet, imperdiet a velit. Integer blandit magna mi, commodo sagittis lacus interdum ac. In porttitor ut lacus ac varius. Nullam sodales ac purus in pulvinar.')
 
     # time.sleep(3)
@@ -50,6 +54,7 @@ try :
     #upload file
     driver.find_element_by_xpath('//*[@id="input-attachment"]').send_keys('/Users/victoreric/selenium/images/news.doc') 
     driver.find_element_by_xpath('//*[@id="input-attachment"]').send_keys('/Users/victoreric/selenium/images/tusur_small.png') 
+    driver.find_element_by_xpath('//*[@id="input-attachment"]').send_keys('/Users/victoreric/selenium/images/error_passw.png')
     time.sleep(1)
 
     #send message
@@ -67,6 +72,7 @@ try :
     driver.close()
     driver.quit()
 
+# при ошибке в ЖЦ теста необходимо сделать скриншот и сохранить html страницы
 except :
     raw_filename="/Users/victoreric/selenium/images/error_passw.png"
     driver.save_screenshot(raw_filename)
